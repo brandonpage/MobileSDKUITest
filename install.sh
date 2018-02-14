ee24f2704ca2e052223ce8e8e00cafcd2d980e74
brandonpage <bpage1@salesforce.com>
2018-02-14 13:58:08 -0800

    Fix install script.

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ git submodule init
 git submodule sync
 git submodule update --init --recursive
 
-cd SalesforceMobileSDK-Package && git checkout packagename && install.js
 npm install -g ios-sim
 npm install -g cordova@7.0.0
+
+cd SalesforceMobileSDK-Package
+git checkout packagename
+./install.js
