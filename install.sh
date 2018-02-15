04bc5d032ef4e23695553bd7829498824083adcd
brandonpage <bpage1@salesforce.com>
2018-02-14 16:19:18 -0800

    fix submodule

--- install.sh
+++ install.sh
@@ -7,13 +7,6 @@ git submodule update --init --recursive
 sudo npm install -g ios-sim
 sudo npm install -g cordova@7.0.0
 
-pwd
-ls
-
 cd SalesforceMobileSDK-Package
-
-pwd
-ls
-
 git checkout packagename
-./install.js
+node ./install.js
