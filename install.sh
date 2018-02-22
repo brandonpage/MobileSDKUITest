49ad90d17dcb4a4a764ca1ada9a2b4be16decf5c
brandonpage <bpage1@salesforce.com>
2018-02-22 13:23:22 -0800

    Fix Android setup issues.

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ git submodule init
 git submodule sync
 git submodule update --init --recursive
 
-npm install -g cordova@7.0.0
+sudo apt-get update
+sudo apt-get install libqt5widgets5
+sudo npm install -g shelljs@0.7.0
+sudo npm install -g cordova@7.0.0
+cordova telemetry off
 
 cd SalesforceMobileSDK-Package
 git checkout packagename
