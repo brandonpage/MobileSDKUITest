7a90ad82be171e576910672f31a69b70391c2595
brandonpage <bpage1@salesforce.com>
2018-03-19 14:46:49 -0700

    Fix intall script with os check

--- install.js
+++ install.js
@@ -9,14 +9,11 @@ var repoUrlWithBranch = packageJson.testAppDependencies[packagerDependency];
 var parts = repoUrlWithBranch.split('#'), repoUrl = parts[0], branch = parts.length > 1 ? parts[1] : 'master';
 execSync('git clone --branch ' + branch + ' --single-branch --depth 1 ' + repoUrl, {stdio:[0,1,2]});
 
-console.log('Installing npm dependencies');
-if(process.env.CIRCLECI) {
+console.log('Installing NPM Dependencies');
+if(process.platform === 'linux') {
     execSync('sudo apt-get update');
     execSync('sudo apt-get install libqt5widgets5');
-    execSync('sudo npm install -g', {stdio:[0,1,2]});
 }
-else {
-    execSync('npm install -g', {stdio:[0,1,2]});
-}
+execSync('npm install -g', {stdio:[0,1,2]});
 execSync('cordova telemetry off');
 execSync('gem install --no-document fastlane');
