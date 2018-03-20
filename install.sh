b5d7db3421968bbde8692cc5793b44c04c573d88
brandonpage <bpage1@salesforce.com>
2018-03-20 07:56:22 -0700

    conditional sudo for npm

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install libqt5widgets5
 fi
 
-git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
 if [[ -v "CIRCLECI" ]]; then
-    sudo npm install shelljs@0.7.0
     sudo npm install -g cordova@7.0.0
     sudo npm install -g sfdx-cli
 else
-    npm install shelljs@0.7.0
     npm install -g cordova@7.0.0
     npm install -g sfdx-cli
 fi
 
 cordova telemetry off
 gem install --no-document fastlane
+
+git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
+cd SalesforceMobileSDK-Package && node ./install.js
