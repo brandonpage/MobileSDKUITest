b0187e53151da7d3fed292ee0c0b9494384e87ae
brandonpage <bpage1@salesforce.com>
2018-03-20 07:31:51 -0700

    conditional sudo for npm

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ fi
 
 git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
 if [[ -v "CIRCLECI" ]]; then
-    sudo npm install -g
+    sudo npm install -g shelljs@0.7.0
+    sudo npm install -g cordova@7.0.0
+    sudo npm install -g sfdx-cli
 else
-    npm install -g
+    npm install -g shelljs@0.7.0
+    npm install -g cordova@7.0.0
+    npm install -g sfdx-cli
 fi
 
 cordova telemetry off
