a51f9e99c156d4dc8ac79b77b66612c89c083383
brandonpage <bpage1@salesforce.com>
2018-03-19 21:38:40 -0700

    Simlified shell script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#if [[ "$OSTYPE" == "linux-gnu" ]]; then
-#    sudo apt-get update
-#    sudo apt-get install libqt5widgets5
-#fi
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo apt-get update
+    sudo apt-get install libqt5widgets5
+fi
 
 git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
 npm install -g
