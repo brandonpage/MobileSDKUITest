b55ed772df2cbbe342df5dada0e19cc503867321
brandonpage <bpage1@salesforce.com>
2018-03-19 23:05:39 -0700

    Simlified shell script

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install libqt5widgets5
 fi
 
+gem install --no-document fastlane
+if [[ -v "CIRCLECI" ]]; then
+    su -
+fi
+
 git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
 npm install -g
 cordova telemetry off
-gem install --no-document fastlane
